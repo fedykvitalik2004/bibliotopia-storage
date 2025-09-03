92830d5cc09c7f8d1ac2ee24f40255e48f90d773
fedykvitalik2004 <vitalii.fedyk.it.2021@lpnu.ua>
2025-09-03 21:44:45 +0300

    feat: introduce minio config

--- code/boot/src/main/java/org/vitalii/fedyk/BootApplication.java
+++ code/boot/src/main/java/org/vitalii/fedyk/BootApplication.java
@@ -1,9 +1,12 @@
-package org.vitalii.fedyk.boot;
+package org.vitalii.fedyk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.vitalii.fedyk.minio.usecase.MinIoProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(MinIoProperties.class)
 public class BootApplication {
 
   public static void main(String[] args) {
--- code/boot/src/main/java/org/vitalii/fedyk/boot/Utils.java
+++
@@ -1,8 +0,0 @@
-package org.vitalii.fedyk.boot;
-
-/** Added. */
-public class Utils {
-  public static int add(int a) {
-    return 3 + a;
-  }
-}
