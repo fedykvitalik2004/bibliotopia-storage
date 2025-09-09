abd3d5b5e4162a7cb42213b4b62bbdf9064e7f40
fedykvitalik2004 <vitalii.fedyk.it.2021@lpnu.ua>
2025-09-09 21:43:06 +0300

    chore: update poms

--- code/domain/src/main/java/org/vitalii/fedyk/minio/model/MinIoUploadResultVO.java
+++
@@ -1,7 +0,0 @@
-package org.vitalii.fedyk.minio.model;
-
-import lombok.Builder;
-
-@Builder
-public record MinIoUploadResultVO(String bucketName, String objectName, String url, long size, String contentType) {
-}
