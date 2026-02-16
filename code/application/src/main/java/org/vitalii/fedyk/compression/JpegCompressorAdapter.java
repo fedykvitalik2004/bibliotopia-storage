2c41db3896f9aebf70c204d4aa90083ceea6e675
fedykvitalik2004 <vitalii.fedyk.it.2021@lpnu.ua>
2026-02-16 23:32:56 +0200

    feature: introduce compression

---
+++ code/application/src/main/java/org/vitalii/fedyk/compression/JpegCompressorAdapter.java
@@ -0,0 +1,25 @@
+package org.vitalii.fedyk.compression;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Set;
+import net.coobird.thumbnailator.Thumbnails;
+import org.springframework.stereotype.Component;
+
+/** Adapter implementation of {@link ImageCompressorPort} for JPEG images. */
+@Component
+public class JpegCompressorAdapter implements ImageCompressorPort {
+  @Override
+  public byte[] compress(InputStream inputStream) throws IOException {
+    try (ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+      Thumbnails.of(inputStream).scale(1.0).outputQuality(0.9).toOutputStream(os);
+      return os.toByteArray();
+    }
+  }
+
+  @Override
+  public Set<String> getSupportedNames() {
+    return Set.of("jpeg", "jpg");
+  }
+}
